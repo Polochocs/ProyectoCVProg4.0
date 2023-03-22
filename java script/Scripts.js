98e268145f503c0fb354ea9e589273ffb8131f21
Polochocs <126638534+Polochocs@users.noreply.github.com>
2023-03-21 21:55:57 -0300

    Add files via upload

---
+++ java script/Scripts.js
@@ -0,0 +1,36 @@
+
+function enBlanco1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function enBlanco2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enBlanco3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enBlanco4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
